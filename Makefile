54c4076a48e9e07f4c2f73191093d8d1ed8bf0c5
Michał Górny <mgorny@gentoo.org>
2018-01-26 20:28:12 +0100

    Script and the Makefile

---
+++ Makefile
@@ -0,0 +1,38 @@
+PORTDIR = $(shell portageq get_repo_path / gentoo)
+GNUPLOT_PREAMBLE = set terminal png size 800,600;
+
+OUT_PNG = by-filename.png by-csum.png by-csum2.png
+OUT_TXT = by-filename.txt by-sha512sum.txt by-sha512sum2.txt \
+	by-filename-checksum.txt by-filename-checksum2.txt
+MAN_TXT = all-manifests.txt
+
+all: $(OUT_PNG)
+clean:
+	rm -f $(OUT_PNG) $(OUT_TXT) $(MAN_TXT)
+
+$(MAN_TXT):
+	find $(PORTDIR) -name Manifest -exec cat {} + | sort -u > $@
+
+by-filename.txt: $(MAN_TXT)
+	./stats.py filename < $< > $@
+
+by-filename.png: by-filename.txt
+	gnuplot -e '$(GNUPLOT_PREAMBLE) set output "$@"; plot "$<" using 2:xticlabels(1)'
+
+by-sha512sum.txt: $(MAN_TXT)
+	./stats.py sha512sum < $< > $@
+
+by-sha512sum2.txt: $(MAN_TXT)
+	./stats.py sha512sum2 < $< > $@
+
+by-filename-checksum.txt: $(MAN_TXT)
+	./stats.py filename_checksum < $< > $@
+
+by-filename-checksum2.txt: $(MAN_TXT)
+	./stats.py filename_checksum2 < $< > $@
+
+by-csum.png: by-sha512sum.txt by-filename-checksum.txt
+	gnuplot -e '$(GNUPLOT_PREAMBLE) set output "$@"; plot "by-sha512sum.txt" using 2:xticlabels(1), "by-filename-checksum.txt" using 2:xticlabels(1)'
+
+by-csum2.png: by-sha512sum2.txt by-filename-checksum2.txt
+	gnuplot -e '$(GNUPLOT_PREAMBLE) set output "$@"; plot "by-sha512sum2.txt" using 2:xticlabels(1), "by-filename-checksum2.txt" using 2:xticlabels(1)'
---
+++ stats.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import collections
+import hashlib
+import math
+import sys
+
+
+class Group(object):
+    count = 0
+    size = 0
+
+
+def makedict(it):
+    it = iter(it)
+    while True:
+        try:
+            i = next(it)
+        except StopIteration:
+            return
+        j = next(it)
+        yield (i, j)
+
+
+class File(object):
+    def __init__(self, l):
+        ftype, self.filename, size, *hashes = l.split()
+        assert ftype == 'DIST'
+        self.size = int(size)
+        self.hashes = dict(makedict(hashes))
+
+
+class keys(object):
+    @staticmethod
+    def filename(f):
+        return f.filename[0]
+
+    @staticmethod
+    def sha512sum(f):
+        return f.hashes['SHA512'][0:1]
+
+    @staticmethod
+    def filename_checksum(f):
+        h = hashlib.sha512()
+        h.update(f.filename.encode('utf8'))
+        return h.hexdigest()[0:1]
+
+    @staticmethod
+    def sha512sum2(f):
+        return f.hashes['SHA512'][0:2]
+
+    @staticmethod
+    def filename_checksum2(f):
+        h = hashlib.sha512()
+        h.update(f.filename.encode('utf8'))
+        return h.hexdigest()[0:2]
+
+
+groups = collections.defaultdict(Group)
+group_func = getattr(keys, sys.argv[1])
+
+for l in sys.stdin:
+    f = File(l)
+    g = group_func(f)
+    groups[g].count += 1
+    groups[g].size += f.size
+
+for k, g in sorted(groups.items()):
+    print('"%s"\t%d' % (k, g.count))
